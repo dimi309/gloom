214462e94ebcd60862ab9d09675cc2c2b5eefeb2
dimi309 <dimi309@users.noreply.github.com>
2021-09-30 22:41:38 +0200

    OpenGL support added and made default.

--- conanfile.py
+++ conanfile.py
@@ -2,14 +2,23 @@ from conans import ConanFile, CMake, tools
 
 class GloomGameConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch", "cppstd"
+    options = {"vulkan": [True, False]}
+    default_options = {"vulkan": False}
     generators = "cmake", "CMakeToolchain", "VirtualBuildEnv", "VirtualRunEnv"
     requires = "small3d/master"
 
+    def config_options(self):
+        self.options["small3d"].vulkan = self.options.vulkan
+        
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def imports(self):
-        self.copy("*.spv", dst="", src="")
+        if self.options.vulkan:
+            self.copy("*.spv", dst="", src="")
+        else:
+            self.copy("*.vert", dst="", src="")
+            self.copy("*.frag", dst="", src="")
         
