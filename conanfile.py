ace5210f16ac48976a269c5264ef770f6c4e537b
dimi309 <dimi309@users.noreply.github.com>
2023-03-03 21:10:20 +0100

    Only using OpenGL for now.

--- conanfile.py
+++ conanfile.py
@@ -5,25 +5,16 @@ import os
 
 class GloomGameConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    options = {"vulkan": [True, False]}
-    default_options = {"vulkan": False}
     requires = "small3d/master"
-
-    def config_options(self):
-        self.options["small3d"].vulkan = self.options.vulkan
 
     def generate(self):
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
-        if not self.options.vulkan:
-            tc.variables["SMALL3D_OPENGL"] = True
+        tc.variables["SMALL3D_OPENGL"] = True
         tc.generate()
-        if self.options.vulkan:
-            copy(self, "*spv", self.dependencies["small3d"].cpp_info.bindirs[0], os.path.join(self.build_folder, "bin"))
-        else:
-            copy(self, "*vert", self.dependencies["small3d"].cpp_info.bindirs[0], os.path.join(self.build_folder, "bin"))
-            copy(self, "*frag", self.dependencies["small3d"].cpp_info.bindirs[0], os.path.join(self.build_folder, "bin"))
+        copy(self, "*vert", self.dependencies["small3d"].cpp_info.bindirs[0], os.path.join(self.build_folder, "bin"))
+        copy(self, "*frag", self.dependencies["small3d"].cpp_info.bindirs[0], os.path.join(self.build_folder, "bin"))
         
     def build(self):
         cmake = CMake(self)
